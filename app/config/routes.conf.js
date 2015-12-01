9a0b20fc729ba49eb2c45cf5ea26decef6f4403d
Michael Phelan <phelan.map@gmail.com>
2015-12-01 02:39:38 +0000

    modifcations

--- app/config/routes.conf.js
+++
@@ -1,6 +0,0 @@
-angular.module('ct')
-  .config(Routes);
-Routes.$inject  = [ '$stateProvider','$urlRouterProvider','APP_PROPERTIES'];
-function Routes($stateProvider,$urlRouterProvider,APP_PROPERTIES) {
-    $urlRouterProvider.otherwise(APP_PROPERTIES['state.base.path'])
-}
